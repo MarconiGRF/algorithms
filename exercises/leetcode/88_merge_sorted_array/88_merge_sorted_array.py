b456537605f195004f4448c4fbf3378e0408a834
Marconi Gomes <marconi.gomes7@gmail.com>
2024-05-05 11:33:14 -0300

    Partially fixes the 88 problem of leetcode.com solution

--- exercises/leetcode/88_merge_sorted_array/88_merge_sorted_array.py
+++ exercises/leetcode/88_merge_sorted_array/88_merge_sorted_array.py
@@ -3,25 +3,24 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        answ = []
-
         i = 0
         j = 0
-        while i < m and j < n:
-            if nums1[i] < nums2[j]:
-                nums1.append(nums1[i])
+        while i < len(nums1) and j < n:
+            if nums1[i] <= nums2[j] and nums1[i] != 0:
                 i += 1
             else:
-                nums1.append(nums2[j])
-                j += 1
-
-        if i < m:
-            while i < m:
-                nums1.append(nums1[i])
+                nums1.insert(i, nums2[j])
                 i += 1
-        if j < n:
-            while j < n:
-                nums1.append(nums2[j])
                 j += 1
 
-        nums1 = answ
+        k = len(nums1) - 1
+        while k >= 0:
+            if nums1[k] == 0:
+                nums1.pop()
+                k -= 1
+            else:
+                break
+
+if __name__ == '__main__':
+    # Solution.merge(None, [1,2,3,0,0,0], 3, [2,5,6], 3)
+    Solution.merge(None, [1], 1, [], 0)
